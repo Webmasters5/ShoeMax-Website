0593063c29d21025b92a4654451c34d1fa3de32d
Mitilesh Ramanna <mitilesh.ramanna1@umail.uom.ac.mu>
2025-09-09 17:58:51 +0400

    Set up products view and url

--- products/views.py
+++ products/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def product_details(request, product_id):
+    return HttpResponse(f'Product {product_id} details')
