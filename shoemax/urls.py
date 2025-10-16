0b9e5b7a55e857690373c770218ba5698d42e3ed
ourvashi20 <ourvashi.ruggee1@umail.uom.ac.mu>
2025-10-16 23:12:47 +0400

    "Made necessary changes"

--- shoemax/urls.py
+++ shoemax/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for shoemax project.
+URL configuration for ShoeMax project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('part.urls')),
+    path('',include('homepage.urls')),
+    path('home/',include('homepage.urls')),
+    path('products/', include('products.urls')),
+    path('core/',include('core.urls')),
 ]
--- shoemax/wsgi.py
+++ shoemax/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shoemax project.
+WSGI config for ShoeMax project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoemax.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ShoeMax.settings')
 
 application = get_wsgi_application()
