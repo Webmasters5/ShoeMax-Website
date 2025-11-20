0caa999e3c237fb7bd240204b953d6647f1092a1
Mitilesh Ramanna <mitilesh.ramanna1@umail.uom.ac.mu>
2025-11-20 09:24:37 +0400

    Fixed style of some elements and changed reverse url names

--- cart/urls.py
+++ cart/urls.py
@@ -5,8 +5,8 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', views.cart_summary, name="summary"),
-    path('add/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('add/', views.add_to_cart, name='add_item'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_item'),
     path('checkout/', views.checkout, name="checkout"),
     path('search/', views.search_view, name="search"),
     path('update/<int:item_id>/', views.update_quantity, name='update_quantity'), 
