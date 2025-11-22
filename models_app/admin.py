33f28b45e3be0f5e39baf2bb6d28c59292934424
Mitilesh Ramanna <mitilesh.ramanna1@umail.uom.ac.mu>
2025-11-22 21:04:32 +0400

    Created model admin classes

--- models_app/admin.py
+++ models_app/admin.py
@@ -1,24 +1,84 @@
 from django.contrib import admin
 from models_app import models
 
-# Register your models here.
-
 class OrderItemInline(admin.TabularInline):
     model = models.OrderItem
     extra = 1
 
-class OrderAdmin(admin.ModelAdmin):
-    inlines = [OrderItemInline]
+class ShoeImageInline(admin.TabularInline):
+    model = models.ShoeImage
+    extra = 1
+    readonly_fields = ()
+    fields = ('image', 'alt_text')
 
-admin.site.register(models.Shoe)
+class ShoeVariantInline(admin.TabularInline):
+    model = models.ShoeVariant
+    extra = 1
+    fields = ('color', 'size', 'stock', 'sku')
+
+class ShoeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'brand', 'category', 'gender', 'price')
+    list_filter = ('brand', 'category', 'gender')
+    search_fields = ('name', 'description', 'brand__name', 'variants__sku')
+    inlines = [ShoeImageInline, ShoeVariantInline]
+    autocomplete_fields = ('brand',)
+    save_on_top = True
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phone', 'mobile', 'theme_preference')
+    search_fields = ('user__username', 'user__email', 'phone')
+    raw_id_fields = ('user',)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'order_date', 'status', 'total_price')
+    list_filter = ('status', 'order_date')
+    search_fields = ('customer__user__username', 'id')
+    inlines = [OrderItemInline]
+    raw_id_fields = ('customer',)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product_name', 'quantity', 'price')
+    search_fields = ('product_name', 'order__id')
+    raw_id_fields = ('order',)
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'message', 'is_read', 'created_at', 'related_order')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('customer__user__username', 'message')
+    raw_id_fields = ('customer', 'related_order')
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('title', 'rating', 'date_added', 'order_item')
+    search_fields = ('title', 'order_item__product_name')
+
+class SiteAdminAdmin(admin.ModelAdmin):
+    list_display = ('admin_id', 'user', 'role')
+    raw_id_fields = ('user',)
+
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'shoe', 'date_added')
+    search_fields = ('customer__user__username', 'shoe__name')
+    raw_id_fields = ('customer', 'shoe')
+
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'variant', 'quantity')
+    search_fields = ('customer__user__username', 'variant__sku')
+    raw_id_fields = ('customer', 'variant')
+
+admin.site.register(models.Shoe, ShoeAdmin)
 admin.site.register(models.ShoeImage)
 admin.site.register(models.ShoeVariant)
-admin.site.register(models.Brand)
-admin.site.register(models.Customer)
+admin.site.register(models.Brand, BrandAdmin)
+admin.site.register(models.Customer, CustomerAdmin)
 admin.site.register(models.Order, OrderAdmin)
-admin.site.register(models.OrderItem)
-admin.site.register(models.Notification)
-admin.site.register(models.Review)
-admin.site.register(models.Admin) 
-admin.site.register(models.WishlistItem)
-admin.site.register(models.CartItem)
+admin.site.register(models.OrderItem, OrderItemAdmin)
+admin.site.register(models.Notification, NotificationAdmin)
+admin.site.register(models.Review, ReviewAdmin)
+admin.site.register(models.Admin, SiteAdminAdmin)
+admin.site.register(models.WishlistItem, WishlistItemAdmin)
+admin.site.register(models.CartItem, CartItemAdmin)
