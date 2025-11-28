5c17d4b51aa696485ebae8257b6016dcdf8d2ee2
kritanandsinghjaunky-source <kritanandsingh.jaunky@umail.uom.ac.mu>
2025-11-28 21:22:57 +0400

    forgot password updated and works with registered email.

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from .views import forgot_password_view
+from django.contrib.auth import views as auth_views
 
 app_name='core'
 
@@ -11,5 +12,10 @@ urlpatterns=[
     path('login/',views.log_in,name='login'),
     path('login/',views.logOut,name='logout'),
     path('signup/',views.signup,name='signup'),
-    path('forgot/',views.forgot_password_view,name='forgot')
+    # path('forgot/',views.forgot_password_view,name='forgot'),
+
+    path('password_reset/', forgot_password_view.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
